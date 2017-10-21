b21d1835aeeff06cc01613275ad26a844f8c95a0
Pyntia <vadim.pyntya@gmail.com>
2017-10-22 00:14:21 +0300

    Simple chromedriver test

---
+++ src/test/java/Tests/SimpleWebDriverTest.java
@@ -0,0 +1,26 @@
+package Tests;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class SimpleWebDriverTest {
+
+    @Test
+    public void driverTest() throws InterruptedException {
+        // Optional, if not specified, WebDriver will search your path for chromedriver.
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com/xhtml");
+        Thread.sleep(5000);  // Let the user actually see something!
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        Thread.sleep(5000);  // Let the user actually see something!
+        driver.quit();
+    }
+}
